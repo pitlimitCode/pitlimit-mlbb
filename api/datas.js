6de37b007d14aa5bc78892b276f3138eea3a32b8
pitlimitCode <prabow14@gmail.com>
2022-12-05 14:34:36 +0700

    'quickRun'

--- api/datas.js
+++ api/datas.js
@@ -1,27 +1,27 @@
 const row1 = []; const row2 = []; const row3 = []; const row4 = []; const row5 = [];
 
 
-const date = '26 November 2022'
+const date = '03 Desember 2022'
 
 row1.push("joy")
-row1.push("fredrinn")
+row1.push("chou")
+row1.push("alice")
 row1.push("badang")
-row1.push("argus")
-row1.push("phoveus")
+row1.push("fredrinn")
 
 row2.push("martis")
-row2.push("leomord")
 row2.push("kaja")
+row2.push("leomord")
+row2.push("fanny")
 row2.push("aulus")
-row2.push("aldous")
 
 row3.push("gusion")
 row3.push("yve")
+row3.push("faramis")
 row3.push("zhask")
-row3.push("lylia")
 row3.push("lunox")
 
-row4.push("chou")
+row4.push("franco")
 row4.push("carmilla")
 row4.push("lolita")
 row4.push("diggie")
@@ -31,7 +31,7 @@ row5.push("wanwan")
 row5.push("lesley")
 row5.push("kimmy")
 row5.push("clint")
-row5.push("irithel")
+row5.push("melissa")
 
 const heroesRank = 
 [
